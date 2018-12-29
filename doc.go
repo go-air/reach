7e90dc5b5f87b6709544584643c667047f96c9b4
Scott <wsc@iri-labs.com>
2018-12-29 12:18:48 +0100

    prettier godoc

--- doc.go
+++ doc.go
@@ -10,18 +10,18 @@
 // latches in S. The set of initial states is defined by the initial values of
 // latches in S.
 //
-// The root package, reach, is centered around a few data structures to to aid
-// in coordinating checkers.
+//   The root package, reach, is centered around a few data structures to to aid
+//   in coordinating checkers.
 //
-// 4. `Output`, which is the output of a checker from analyzing a logic.S and
-// a, or some, bad states.
+//   1. `Output`, which is the output of a checker from analyzing a logic.S and
+//   a, or some, bad states.
 //
-// 1. `Result`, which is the result of analysing a single reachability query
-// with one set of bad states represented by a z.Lit in a logic.S.
+//   2. `Result`, which is the result of analysing a single reachability query
+//   with one set of bad states represented by a z.Lit in a logic.S.
 //
-// 3. `Primer` which can find `x'` given `x` for latches and properties.
+//   3. `Primer` which can find `x'` given `x` for latches and properties.
 //
-// 2. `Trace`, which is a trace of a sequential logic system.
+//   4. `Trace`, which is a trace of a sequential logic system.
 //
 // These are concepts related to coordination of checkers. Various checkers are
 // found in the subpackages of reach.
